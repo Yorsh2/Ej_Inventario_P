a3e34a5a89ba35672be3f4e1b3d4069180f167aa
Jorge2 <147229760+Yorsh2@users.noreply.github.com>
2024-02-22 09:03:17 -0700

    Update InvRoutes.js
    Cambio rutas

--- rutas/InvRoutes.js
+++ rutas/InvRoutes.js
@@ -3,52 +3,45 @@ const router = express.Router();
 const invcontroller = require('../controllers/InvController');
 const authutils = require('../Middleware/authUtils');
 
-let cadena=[];
 
-/*
 function authenticate(req, res, next) {
-    const token = req.headers.authorization;
-    if(!token){
-        return res.status(401).json({error: "Unauthorized"});
-    }
-    const decodedtoken = authutils.verifyToken(token);
-    if(!decodedtoken){
-        return res.status(401).json({error: 'Unauthorized'});
-    }
-    req.user = decodedtoken;
-    next();
+  const token = req.headers.authorization;
+  if(!token){
+      return res.status(401).json({error: "Unauthorized"});
+  }
+  const decodedtoken = authutils.verifyToken(token);
+  if(!decodedtoken){
+      return res.status(401).json({error: 'Unauthorized'});
+  }
+  req.user = decodedtoken;
+  next();
 }
 
-router.get('/', authenticate, (req, res)=> {
-    //const tasks = 
-    res.json(taskcontroller.getAllTasks());
-});
-*/
   //1. Agregar nuevo producto
-  router.post('/', (req, res)=> {
+  router.post('/', authenticate, (req, res)=> {
       const { nombre, descripcion, cantidad, precio } = req.body;
       const newInv = invcontroller.createInv(nombre, descripcion, cantidad, precio);
       res.status(201).json(newInv);
   });
   //2. Obtener todos los productos
-  router.get('/', (req,res) => {
+  router.get('/', authenticate, (req,res) => {
     res.json(invcontroller.getAllInv());
   });
 
   //3. Obtener un producto por su ID
-  router.get('/:id',(req, res)=> {
+  router.get('/:id', authenticate, (req, res)=> {
     const idProducto = parseInt(req.params.id);
     const productoEncontrado = invcontroller.getInvID(idProducto);
 
     if (productoEncontrado) {
       res.json(productoEncontrado);
     } else {
-      res.status(404).json({ error: 'No se encontro la tarea 1' });
+      res.status(404).json({ error: 'No se encontro la tarea' });
     }
   });
 
   //4. Actualizar la información de un producto
-  router.put('/:id',(req,res)=>{
+  router.put('/:id', authenticate, (req,res)=>{
     const idProducto = parseInt(req.params.id) //id a numero
     const { nombre, descripcion, cantidad, precio } = req.body;
     const productoEncontrado = invcontroller.updateInv(idProducto, nombre, descripcion, cantidad, precio); 
@@ -60,7 +53,7 @@ router.get('/', authenticate, (req, res)=> {
   });
 
   //5. Eliminar un producto
-  router.delete('/:id',(req,res)=>{
+  router.delete('/:id', authenticate, (req,res)=>{
     const idProducto = parseInt(req.params.id);
     const productoEncontrado = invcontroller.deleteInv(idProducto);
     if (productoEncontrado) {
@@ -75,7 +68,7 @@ router.get('/', authenticate, (req, res)=> {
   });
 
   //6. Buscar productos por su nombre
-  router.get('/buscar/:nombre', (req, res) => {
+  router.get('/buscar/:nombre', authenticate, (req, res) => {
     const { nombre } = req.params;
     const productoEncontrado = invcontroller.getInvNombre(nombre);
     console.log(productoEncontrado);
@@ -87,17 +80,26 @@ router.get('/', authenticate, (req, res)=> {
   });
 
   //7. Cálculo del valor total de inventario
-  router.get('/inventario/total', (req, res) => {
+  router.get('/costo/total', authenticate, (req, res) => {
     const valorTotal = invcontroller.getValorTotal();
     res.json({ valorTotal });
   });
 
   //8. Ordenar productos por precio
-  router.get('/ordenar/precio', (req, res) => {
+  router.get('/ordenar/precio', authenticate, (req, res) => {
     const { producto } = req.query; // Expect 'asc' or 'desc'
     const ordProducto = invcontroller.ordenarPrecio(producto);
     res.json(ordProducto);
   });
+
+  //9. Filtrar productos por cantidad en stock
+  router.get('/filtrar/producto', authenticate, (req, res) => {
+    const { stock } = req.query;
+    const productoFiltrado = invcontroller.filtrarPorStock(stock);
+    res.json(productoFiltrado);
+});
+
+module.exports = router;
 
   //9. Filtrar productos por cantidad en stock
   router.get('/filtrar/producto', (req, res) => {
